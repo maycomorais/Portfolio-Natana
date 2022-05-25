5a4a435b30f95ba9e12f55d75214c871f183576e
Maycon Morais <97128625+maycomorais@users.noreply.github.com>
2022-05-25 19:04:52 -0300

    Natana2

--- script.js
+++ script.js
@@ -49,13 +49,13 @@ $(document).ready(function(){
 
 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Front-end", "Back-end", "Full Stack", "Designer"],
+        strings: ["Enfermeira", "Tricologista", "Esteticista"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Front-end", "Back-end", "Full Stack", "Designer"],
+        strings: ["Enfermeira", "Tricologista", "Esteticista"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
